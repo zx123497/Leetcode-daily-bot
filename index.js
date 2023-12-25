d5cd18563144ec97cb3cf435892a8b72b515f73f
johnnychang25678 <zjps3407@gmail.com>
2023-12-24 16:53:46 -0800

    format

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 const schedule = require('node-schedule');
 const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
+const { authenticate } = require('@google-cloud/local-auth');
+const { google } = require('googleapis');
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -38,24 +38,24 @@ query questionOfToday {
 
 // We can pass the JSON response as an object to our createTodoistTask later.
 const fetchDailyCodingChallenge = async () => {
-    console.log(`Fetching daily coding challenge from LeetCode API.`)
+  console.log(`Fetching daily coding challenge from LeetCode API.`)
 
-    const init = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY }),
-    }
+  const init = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: DAILY_CODING_CHALLENGE_QUERY }),
+  }
 
-    const response = await fetch(LEETCODE_API_ENDPOINT, init)
-    return response.json()
+  const response = await fetch(LEETCODE_API_ENDPOINT, init)
+  return response.json()
 }
 
 const getDailyCodingChallenge = async () => {
-    const question = await fetchDailyCodingChallenge();
-    const questionTitle = question.data.activeDailyCodingChallengeQuestion.question.title;
-    const questionLink = question.data.activeDailyCodingChallengeQuestion.link;
-    const questionDate = question.data.activeDailyCodingChallengeQuestion.date;
-    return {questionTitle, questionLink, questionDate};
+  const question = await fetchDailyCodingChallenge();
+  const questionTitle = question.data.activeDailyCodingChallengeQuestion.question.title;
+  const questionLink = question.data.activeDailyCodingChallengeQuestion.link;
+  const questionDate = question.data.activeDailyCodingChallengeQuestion.date;
+  return { questionTitle, questionLink, questionDate };
 }
 
 
@@ -76,95 +76,90 @@ const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
  * @return {Promise<OAuth2Client|null>}
  */
 async function loadSavedCredentialsIfExist() {
-    try {
-      const content = await fs.readFile(TOKEN_PATH);
-      const credentials = JSON.parse(content);
-      return google.auth.fromJSON(credentials);
-    } catch (err) {
-      return null;
-    }
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
   }
+}
 
-  /**
- * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
+/**
+* Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+*
+* @param {OAuth2Client} client
+* @return {Promise<void>}
+*/
 async function saveCredentials(client) {
-    const content = await fs.readFile(CREDENTIALS_PATH);
-    const keys = JSON.parse(content);
-    const key = keys.installed || keys.web;
-    const payload = JSON.stringify({
-      type: 'authorized_user',
-      client_id: key.client_id,
-      client_secret: key.client_secret,
-      refresh_token: client.credentials.refresh_token,
-    });
-    await fs.writeFile(TOKEN_PATH, payload);
-  }
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
 
 
-  async function authorize() {
-    let client = await loadSavedCredentialsIfExist();
-    if (client) {
-      return client;
-    }
-    client = await authenticate({
-      scopes: SCOPES,
-      keyfilePath: CREDENTIALS_PATH,
-    });
-    if (client.credentials) {
-      await saveCredentials(client);
-    }
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
     return client;
   }
-
-  async function addQuestion(auth) {
-    const sheets = google.sheets({version: 'v4', auth});
-    const question = await getDailyCodingChallenge();
-    const currentDate = new Date();
-    const day = currentDate.getDate();
-    const month = currentDate.getMonth() + 1;
-    // const date = question.questionDate.split('-')[1] + '/' + question.questionDate.split('-')[2];
-    const list = [[month + '/' + day, "https://leetcode.com" + question.questionLink, '', '', '', '', '']];
-    const resource = {
-      values: list,
-    };
-
-    // add a new row to the sheet
-    const request = [{ 
-        insertDimension : {
-            range : {
-                sheetId : "0",
-                dimension : "ROWS", 
-                startIndex : 2,  
-                endIndex : 3,
-            },
-            inheritFromBefore : false,
-        } 
-    }];
-    const batchUpdateRequest = {requests: request};
-    const res1 = await sheets.spreadsheets.batchUpdate({
-        spreadsheetId: '1hhjTKal_UYRqSLyqW7Ex9SbMIPSUtMZ_bCpjIRYevig',
-        resource: batchUpdateRequest,
-    })
-    
-    // add question to the new row
-    const res2 = await sheets.spreadsheets.values.update({
-      spreadsheetId: '1hhjTKal_UYRqSLyqW7Ex9SbMIPSUtMZ_bCpjIRYevig',
-      range: 'done!A3:G',
-      valueInputOption: 'RAW',
-      resource,
-    });
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
   }
+  return client;
+}
+
+async function addQuestion(auth) {
+  const sheets = google.sheets({ version: 'v4', auth });
+  const question = await getDailyCodingChallenge();
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1;
+  // const date = question.questionDate.split('-')[1] + '/' + question.questionDate.split('-')[2];
+  const list = [[month + '/' + day, "https://leetcode.com" + question.questionLink, '', '', '', '', '']];
+  const resource = {
+    values: list,
+  };
+
+  // add a new row to the sheet
+  const request = [{
+    insertDimension: {
+      range: {
+        sheetId: "0",
+        dimension: "ROWS",
+        startIndex: 2,
+        endIndex: 3,
+      },
+      inheritFromBefore: false,
+    }
+  }];
+  const batchUpdateRequest = { requests: request };
+  const res1 = await sheets.spreadsheets.batchUpdate({
+    spreadsheetId: '1hhjTKal_UYRqSLyqW7Ex9SbMIPSUtMZ_bCpjIRYevig',
+    resource: batchUpdateRequest,
+  })
+
+  // add question to the new row
+  const res2 = await sheets.spreadsheets.values.update({
+    spreadsheetId: '1hhjTKal_UYRqSLyqW7Ex9SbMIPSUtMZ_bCpjIRYevig',
+    range: 'done!A3:G',
+    valueInputOption: 'RAW',
+    resource,
+  });
+}
 
 // update the sheet everyday at 3:01 pm
-let job = schedule.scheduleJob('1 0 16 * * *', async function(){
-    authorize().then(addQuestion).catch(console.error);
+let job = schedule.scheduleJob('1 0 16 * * *', async function () {
+  authorize().then(addQuestion).catch(console.error);
 });
-
-
-
-
-
